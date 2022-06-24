0f4339cd3c9952a4df26369d0e95d13d31f7cb4b
cbaehr <chjbaehr@gmail.com>
2022-06-24 14:58:40 -0400

    change lambda and shift to ridge for no lag model

--- PAR_analysis.R
+++ PAR_analysis.R
@@ -91,7 +91,7 @@ dev.off()
 
 lambda <- exp(-6.5)
 
-mod <- glmnet(x=X_D, y=y_D, lambda=exp(-2.5), alpha=0)
+mod <- glmnet(x=X_D, y=y_D, lambda=lambda, alpha=0)
 
 pred <- mod$a0 +  X_D %*% mod$beta
 
@@ -121,6 +121,57 @@ library(modelsummary)
 f_summary <- All(D) ~ N+Mean+SD+Median+Min+P25+P75+Max
 datasummary(formula=f_summary, data=D, output="../results/sum_stats_Dem.tex")
 
+###
+
+D_nolag <- open[, c("voteshare_D_House", "voteshare_D_Pres", "voteshare_D_Gov",
+                    "gini", "mean_income", "pct_unemp", "pct_hsgrads", "pct_BAdegree", "pct_over60",
+                    "totalpop", "pct_black", "pct_hispanic", "pct_white",
+                    yrdums)]
+
+D <- na.omit(D_nolag)
+X_D <- as.matrix(D[, -1])
+y_D <- D[,1]
+
+lambda <- 10^seq(2, -5, -0.25)
+
+set.seed(1011)
+
+# ridge
+Ridge <- cv.glmnet(x=X_D, y=y_D, lambda=lambda, type.measure="mse", nfolds=5, 
+                   gamma=1, relax=F, alpha=0, standardize=T)
+#plot(Ridge)
+#coef(Ridge)
+
+#lasso
+Lasso <- cv.glmnet(x=X_D, y=y_D, lambda=lambda, type.measure="mse", nfolds=5, 
+                   gamma=1, relax=F, alpha=1, standardize=T)
+#plot(Lasso)
+
+
+pdf("../results/PARmse_Dem_nolag.pdf", width=11, height=6)
+par(mfrow=c(1,2))
+plot(Ridge)
+mtext("Ridge",side=3,line=0,outer=F,cex=1.3, padj=-3.2)
+plot(Lasso, ylab="")
+mtext("Lasso",side=3,line=0,outer=F,cex=1.3, padj=-3.2)
+dev.off()
+
+
+lambda <- exp(-6.5)
+
+mod <- glmnet(x=X_D, y=y_D, lambda=lambda, alpha=1)
+
+pred <- mod$a0 +  X_D %*% mod$beta
+
+openD <- open[rownames(D), ]
+openD$PAR_D <- as.vector(pred)
+openD$PARerror <- openD$voteshare_D_House - openD$PAR_D
+openD <- openD[order(openD$PARerror, decreasing=T), ]
+
+
+write.csv(data.frame(var=rownames(mod$beta), beta=as.vector(round(mod$beta, digits=4))),
+          "../results/PARbetas_Dem_nolag.csv",
+          row.names=F)
 
 
 
