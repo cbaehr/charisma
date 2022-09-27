a945141e9e5b14804a9c5032164878dddcf96f0d
cbaehr <chjbaehr@gmail.com>
2022-09-27 16:14:54 -0400

    construct panel

--- join_election_data.R
+++ join_election_data.R
@@ -220,7 +220,7 @@ seats <- function(x) {
   return(list(seat1=seat1, seat2=seat2))
 }
 
-seatyears <- tapply(senate$sen_RaceDate, INDEX=list(senate$statenm), FUN=seats)
+seatyears <- tapply(senate$RaceDate, INDEX=list(senate$statenm), FUN=seats)
 
 seatyears$illinois$seat2 <- seq(1966, 2022, 6) # this will correct for (ignore) a special election in 1970
 # also ignoring a Georgia special election from 2021
@@ -369,6 +369,8 @@ for(i in 1970:1979) {
 pop1970 <- do.call(rbind, temp)
 
 pop_1970_89 <- do.call(rbind, list(pop1970, pop1980))
+# pull the 1989 population data forward one year to 1990 so we have a measure. Redistricting files with pop start in 1992
+pop_1970_89$year[which(pop_1970_89$year==1989)] <- 1990
 
 countyplus <- merge(countyplus, pop_1970_89, by.x=c("fips", "con_raceYear"), by.y=c("fips", "year"), all.x=T)
 countyplus$county_pop[is.na(countyplus$county_pop)] <- apply(countyplus[is.na(countyplus$county_pop), grep("pop\\.", names(countyplus))], 1, sum)
@@ -382,19 +384,11 @@ pres$mergeid <- paste(tolower(pres$statenm), tolower(pres$countynm), pres$decade
 
 ###
 
-names(countyplus) <- tolower(names(countyplus))
-
-countyplus[, names(countyplus) %in% c("x", "decade", "year")]
-
-
-
-###
 
 which(is.na(countyplus$gov_RepVotesMajorPercent))[50]
 # reference alabama 2004 - no governor election
 for(i in 1:nrow(countyplus)) {
-  
-  i=83
+  #i=82
   
   year <- countyplus$con_raceYear[i]
   county <- countyplus$countynm[i]
@@ -403,36 +397,172 @@ for(i in 1:nrow(countyplus)) {
   
   cty_ind <- countyplus$statenm==state & countyplus$countynm==county
   
-  if(is.na(countyplus$gov_RepVotesMajorPercent[i])) {
-    
+  if(is.na(countyplus$gov_RepVotesMajorPercent[i])) { # check if the major percent vote is missing - implies a governor midterm year
+    # pull all observations for county i in state j in years before current, and for which percent vote is nonmissing
     cands <- countyplus[which(cty_ind & countyplus$con_raceYear<year & !is.na(countyplus$gov_RepVotesMajorPercent)), ]
-    
-    fit <- cands[which.max(cands$con_raceYear),]
-    
-    countyplus[i, grep("gov_", names(countyplus))] <- fit[1, grep("gov_", names(countyplus))]
-    
+    fit <- cands[which.max(cands$con_raceYear),] # identify the observation for the most recent year from the candidates. This is the lagged election we want
+    # replace all missing governor variables with the data from the most previous election
+    countyplus[i, grep("gov_", names(countyplus))] <- fit[1, grep("gov_", names(countyplus))] 
   }
-  if(is.na(countyplus$pres_RepVotesTotalPercent[i])) {
-    
+  if(is.na(countyplus$pres_RepVotesTotalPercent[i])) { # do now for president
     cands <- countyplus[which(cty_ind & countyplus$con_raceYear<year & !is.na(countyplus$pres_RepVotesTotalPercent)), ]
-    
     fit <- cands[which.max(cands$con_raceYear),]
-    
     countyplus[i, grep("pres_", names(countyplus))] <- fit[1, grep("pres_", names(countyplus))]
-    
   }
-  
-  
+  if(is.na(countyplus$sen1_RepVotesMajorPercent[i])) { # do now for senator 1
+    cands <- countyplus[which(cty_ind & countyplus$con_raceYear<year & !is.na(countyplus$sen1_RepVotesMajorPercent)), ]
+    fit <- cands[which.max(cands$con_raceYear),]
+    countyplus[i, grep("sen1_", names(countyplus))] <- fit[1, grep("sen1_", names(countyplus))]
+  }
+  if(is.na(countyplus$sen2_RepVotesMajorPercent[i])) { # do now for senator 2
+    cands <- countyplus[which(cty_ind & countyplus$con_raceYear<year & !is.na(countyplus$sen2_RepVotesMajorPercent)), ]
+    fit <- cands[which.max(cands$con_raceYear),]
+    countyplus[i, grep("sen2_", names(countyplus))] <- fit[1, grep("sen2_", names(countyplus))]
+  }
 }
 
+for(i in c("con_DemVotes", "con_RepVotes", "con_ThirdVotes", "con_OtherVotes", "pres_RepVotes", "pres_DemVotes", "pres_OtherVotes",
+           "sen1_DemVotes", "sen1_RepVotes", "sen1_ThirdVotes", "sen1_OtherVotes", "sen2_DemVotes", "sen2_RepVotes",
+           "sen2_ThirdVotes", "sen2_OtherVotes", "gov_RepVotes", "gov_DemVotes", "gov_ThirdVotes", "gov_OtherVotes")) {
+  countyplus[which(countyplus[,i]=="N/A"), i] <- 0
+  countyplus[,i] <- gsub(",", "", countyplus[,i])
+}
+
+###
+
+names(countyplus) <- tolower(names(countyplus))
+names(countyplus) <- gsub("\\.","_",names(countyplus))
+
+countyplus <- countyplus[, !names(countyplus) %in% c("x", "decade", "year")]
+
+countyplus$con_unopposed <- (countyplus$con_demcandidate=="N/A" & countyplus$con_demstatus=="N/A") | (countyplus$con_repcandidate=="N/A" & countyplus$con_repstatus=="N/A")
+
+###
+
+#View(countyplus[is.na(countyplus$unit_weight),]) 
+# only missing weights cases are in at Large districts or before 1950
+
+View(countyplus[is.na(countyplus$county_pop), ])
+table(countyplus$con_raceyear[is.na(countyplus$county_pop)]) # why are there 3248 missing countypop obs. in 1990?
+table(countyplus$con_raceyear)
+
+#countyplus$con_demvotes <- gsub(",", "", countyplus$con_demvotes)
+#countyplus$con_repvotes <- gsub(",", "", countyplus$con_repvotes)
+
+#countyplus$con_demvotesmajorpercent <- as.numeric(countyplus$con_demvotes)/ (as.numeric(countyplus$con_demvotes)+as.numeric(countyplus$con_repvotes))
+
+#a <- tapply(countyplus$con_demvotesmajorpercent, INDEX=list(countyplus$statenm, countyplus$decade), FUN=mean, na.rm=T)
+
+sum(is.na(countyplus$unit_weight))
+View(countyplus[is.na(countyplus$unit_weight), ]) # bunch of at large elections. Drop these
+# check why we are dropping counties such as Louisville kentucky (CD 3 1988)
+
+#countyplus$con_demvotes <- gsub(",", "", countyplus$con_demvotes)
+#countyplus$con_demvotes <- as.numeric(countyplus$con_demvotes)
+
+###
+
+sum(is.na(countyplus$unit_pop))
+
+for(i in grep("pop_", names(countyplus), value=T)) {
+  countyplus[, paste0("unit_", i)] <- countyplus[,i] * countyplus$unit_weight
+}
+
+vars <- c("pres_repvotes", "pres_demvotes", 
+          "sen1_repvotes", "sen1_demvotes", "sen2_repvotes", "sen2_demvotes",
+          "gov_repvotes", "gov_demvotes")
+for(i in vars) {
+  countyplus[,i] <- gsub(",", "", countyplus[,i])
+  countyplus[,i] <- as.numeric(countyplus[,i])
+  countyplus[,paste0("unit_", i)] <- countyplus[,i] * countyplus$unit_weight
+}
+
+countyplus$unit_con_repvotes <- countyplus$con_repvotes # dont need to rescale for congressional elections
+countyplus$unit_con_demvotes <- countyplus$con_demvotes
+
+cd_level <- aggregate(countyplus[, setdiff(grep("unit_",names(countyplus),value=T), c("unit_weight", "unit_pop"))],
+                      by=list(countyplus$statenm, countyplus$cd, countyplus$con_raceyear),
+                      FUN=sum, na.rm=T)
+# dont have 435 districts for 2010, check on this
+cd_level <- cd_level[which(cd_level$Group.3>1968), ]
+names(cd_level)[1:3] <- c("state", "cd", "year")
+names(cd_level) <- gsub("unit_", "", names(cd_level))
+
+cd_level$con_repshare <- cd_level$con_repvotes / (cd_level$con_demvotes+cd_level$con_repvotes)
+cd_level$con_demshare <- cd_level$con_demvotes / (cd_level$con_demvotes+cd_level$con_repvotes)
+
+cd_level$pres_demshare <- cd_level$pres_demvotes / (cd_level$pres_demvotes+cd_level$pres_repvotes)
+cd_level$pres_repshare <- cd_level$pres_repvotes / (cd_level$pres_demvotes+cd_level$pres_repvotes)
+
+cd_level$gov_repshare <- cd_level$gov_repvotes / (cd_level$gov_demvotes+cd_level$gov_repvotes)
+cd_level$gov_demshare <- cd_level$gov_demvotes / (cd_level$gov_demvotes+cd_level$gov_repvotes)
+
+cd_level$sen1_repshare <- cd_level$sen1_repvotes / (cd_level$sen1_demvotes+cd_level$sen1_repvotes)
+cd_level$sen1_demshare <- cd_level$sen1_demvotes / (cd_level$sen1_demvotes+cd_level$sen1_repvotes)
+
+cd_level$sen2_demshare <- cd_level$sen2_demvotes / (cd_level$sen2_demvotes+cd_level$sen2_repvotes)
+cd_level$sen2_repshare <- cd_level$sen2_repvotes / (cd_level$sen2_demvotes+cd_level$sen2_repvotes)
+
+cd_level <- cd_level[,!grepl("votes", names(cd_level))]
+
+library(modelsummary)
+f_summary <- All(cd_level) ~ N+Mean+SD+Median+Min+P25+P75+Max
+datasummary(formula=f_summary, data=cd_level, output="/Users/christianbaehr/Desktop/sum_stats.tex")
+
+
+load("WDI_vars.RData")
+sumdat <- imf_panel[, !names(imf_panel) %in% keep_vars]
+sumdat <- sumdat[, sort(names(sumdat))]
+
+f_summary <- All(sumdat) ~ N+Mean+SD+Median+Min+P25+P75+Max
+datasummary(formula=f_summary, data=sumdat, output="../results/sum_stats.tex")
 
 
 
+county_store <- countyplus
 
+for(i in vars) {
+  countyplus[,i] <- gsub(",", "", countyplus[,i])
+  countyplus[,i] <- as.numeric(countyplus[,i])
+  countyplus[,i] <- countyplus[,i] * countyplus$unit_weight
+}
 
+test <- aggregate(countyplus[,vars],
+                  by=list(countyplus$con_raceyear, countyplus$statenm, countyplus$cd),
+                  FUN=mean, na.rm=T)
+test <- test[test$Group.1>1970, ]
 
+test$con_repvoteshare <- test$con_repvotes / (test$con_demvotes+test$con_repvotes)
 
+test$decade <- dfloor(test$Group.1)
+test2 <- aggregate(test[,c(vars, "con_repvoteshare")],
+                   by=list(test$Group.2, test$decade), 
+                   FUN=mean,  na.rm=T)
 
+#test2 <- test2[test2$Group.3>1962, ]
+#test2 <- test2[test2$Group.2!="At Large", ]
+
+conrep <- test2[, c("Group.1", "Group.2", "con_repvoteshare")]
+conrep <- reshape(conrep, direction="wide", idvar=c("Group.1"), timevar="Group.2")
+library(xtable)
+print(xtable(conrep, type = "latex"), file = "/Users/christianbaehr/Desktop/repvoteshare.tex")
+
+###
+
+test <- aggregate(countyplus[,vars],
+                  by=list(countyplus$con_raceyear, countyplus$statenm, countyplus$cd),
+                  FUN=sum, na.rm=T)
+test <- test[test$Group.1>1970, ]
+test$decade <- dfloor(test$Group.1)
+
+test2 <- aggregate(test[,c(vars, "unit_pop")],
+                   by=list(test$Group.2, test$decade), 
+                   FUN=sum,  na.rm=T)
+
+conrep <- test2[, c("Group.1", "Group.2", "unit_pop")]
+conrep <- reshape(conrep, direction="wide", idvar=c("Group.1"), timevar="Group.2")
+library(xtable)
+print(xtable(conrep, type = "latex"), file = "/Users/christianbaehr/Desktop/population.tex")
 
 
 
