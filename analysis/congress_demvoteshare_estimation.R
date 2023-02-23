d27a918e08253e94b294cca5d1b35b80e517e817
cbaehr <chjbaehr@gmail.com>
2023-02-22 22:31:04 -0500

    update dem voteshare prediction models

--- analysis/congress_demvoteshare_estimation.R
+++ analysis/congress_demvoteshare_estimation.R
@@ -1,28 +1,35 @@
 
 setwd("/Users/christianbaehr/Dropbox/charisma_project/data/")
 
+options(stringsAsFactors = F)
+
 include.lag=F
 openseats.only=T
 drop.unopposed=F
 include.stateFE=T
-
-#install.packages("readstata13", "glmnet")
-
-#options(expressions = 5e6)
+include.yearFE=T
 
 library(lattice)
 library(glmnet)
 library(dplyr)
+
 set.seed(683750)
 
-cd <- read.csv("working/cd_panel_full.csv")
+cd <- read.csv("working/condistrict_panel_full.csv")
 
-##creating a lag variable within each group (= congressional district)
+###
+
+## creating a lag variable within each group (= congressional district)
+
 cd <- cd[order(cd$statenm, cd$cd, cd$con_raceyear), ]
 
 cd <- cd %>%
   group_by(statenm, cd) %>%
   mutate(lag.con_demshare = dplyr::lag(con_demshare, n=1, default=NA))
+
+###
+
+## if open seats or opposed only dummy is true, then trim the data accordingly
 
 if(openseats.only) {
   cd <- cd[which(cd$con_repstatus!="Incumbent" & cd$dem_inc_count_consecutive==0), ]
@@ -36,7 +43,7 @@ if(drop.unopposed) {
   cd <- cd
 }
 
-hist(cd$con_demshare)
+###
 
 ## selecting features that should be squared (pct - Democrats)
 
@@ -44,6 +51,7 @@ features <- c("pres_demshare", "gov_demshare", "sen1_demshare", "sen2_demshare",
               "pop_male_pct", "pop_over65_pct" , "pop_white_pct", "pop_black_pct"   , "pop_spanishorigin_pct" ,
               "dem_inc_count_consecutive", "dem_inc_count_cumulative", "dem_inc_bin")
 
+## if lagged DV dummy is true, then add to feature vector
 if(include.lag) {
   features <- c("lag.con_demshare", features)
 } else{
@@ -51,137 +59,105 @@ if(include.lag) {
 }
 
 cdfull <- cd[complete.cases(cd[, c("con_demshare", features)]), ] # drop rows with NA
+cdfull$con_raceyear <- as.character(cdfull$con_raceyear) #easier for creating fixed effects
 
-D <- cdfull[, features]
+D <- cdfull[, features] # only keep features for variable transformation
 
 D_names <- names(D)
-D_sq <- data.frame(apply(D[ , D_names], 2, function(x) x^2))
-D_cu <- data.frame(apply(D[ , D_names], 2, function(x) x^3))
+D_sq <- data.frame(apply(D[ , D_names], 2, function(x) x^2)) # squared features
+D_cu <- data.frame(apply(D[ , D_names], 2, function(x) x^3)) # cubed features
 names(D_sq) <- paste0(D_names, "_sq")
 names(D_cu) <- paste0(D_names, "_cu")
-D <- do.call(cbind, list(D, D_sq, D_cu))
+D <- do.call(cbind, list(D, D_sq, D_cu)) # bind linear, squared, and cubic features
 
-## interactions for each pair of covariates
+## interactions for each pair of features
 interactions <- combn(names(D), 2, FUN=paste, collapse='*')
 for(i in interactions) {
   nms <- strsplit(i, "\\*")[[1]]
   D[, i] <- D[, nms[1]] * D[, nms[2]]
 }
 
+# if selected, include state fixed effects
 if(include.stateFE) {
-  statedums <- data.frame(model.matrix(~ 0 + statenm, cdfull)[,-1]) # create state dummies and drop first
+  statedums <- data.frame(model.matrix(~ 0 + statenm, cdfull)[,-1]) # create state dummies and drop first (multicollinearity)
   D <- cbind(D, statedums) #join important covariates with state dummies
 } else {
   D <- D
 }
+
+# if selected, include year fixed effects
+if(include.yearFE) {
+  yeardums <- data.frame(model.matrix(~ 0 + con_raceyear, cdfull)[,-1]) # create state dummies and drop first (multicollinearity)
+  D <- cbind(D, yeardums) #join important covariates with state dummies
+} else {
+  D <- D
+}
+
+###
 
 ## define outcome vector and covariate matrix
 data <- cbind(cdfull[c("con_demshare")], D)
 
 yname <- "con_demshare"
 
-y = data[,c(yname)]
-x = data[, names(data)!="con_demshare"]
-x = model.matrix(as.formula(paste(yname, "~ .", sep="")), data)[,-1]
+y = data[,c(yname)] # vector of y values
+x = data[, names(data)!="con_demshare"] # matrix of X values
+x = model.matrix(as.formula(paste(yname, "~ .", sep="")), data)[,-1] # drop intercept (included automatically in model)
 
 ###
 
-## split out training set
+## create training set indices
 train.indx = sample(1:nrow(data), floor(2/3 * nrow(data)))
 
-cvout.ridge = cv.glmnet(x[train.indx,], y[train.indx], alpha=0)
-pred = predict (cvout.ridge, s="lambda.min", newx=x[-train.indx,])
-terror.ridge = mean((pred - y[-train.indx])^2) #test error 
+## Ridge
+cvout.ridge = cv.glmnet(x[train.indx,], y[train.indx], alpha=0) #fit
+pred.ridge = predict (cvout.ridge, s="lambda.min", newx=x[-train.indx,]) #predict on test set
+error.ridge = pred.ridge - y[-train.indx] #error
+terror.ridge = mean(error.ridge^2) #MSE
+r_sq.ridge <- 1- ( sum(error.ridge^2) / sum((y[-train.indx] - mean(y[-train.indx]))^2) )  #r squared
 
-# Lasso
-cvout.lasso = cv.glmnet(x[train.indx,], y[train.indx], alpha=1)
-pred = predict (cvout.lasso , s="lambda.min", newx=x[-train.indx,])
-terror.lasso = mean((pred - y[-train.indx])^2) # test error 
+## Lasso
+cvout.lasso = cv.glmnet(x[train.indx,], y[train.indx], alpha=1) #fit
+pred.lasso = predict (cvout.lasso , s="lambda.min", newx=x[-train.indx,]) #predict on test set
+error.lasso = pred.lasso - y[-train.indx] #error
+terror.lasso = mean(error.lasso^2) #MSE
+r_sq.lasso <- 1- ( sum(error.lasso^2) / sum((y[-train.indx] - mean(y[-train.indx]))^2) )  #r squared
 
-# Simple lm
-lmout = lm(as.formula(paste(yname, "~ .", sep="")), data=data[train.indx,])
-pred = predict(lmout, newdata=data[-train.indx,])
-terror.lm = mean((pred - y[-train.indx])^2)
+## OLS
+PAR.data <- data[, c("con_demshare", "pres_demshare", "gov_demshare", "sen1_demshare", "sen2_demshare")] #Bob Erikson approach (plus levels )
+#statedums <- data.frame(model.matrix(~ 0 + statenm, cdfull)[,-1]) # create state dummies and drop first (multicollinearity)
+yeardums <- data.frame(model.matrix(~ 0 + con_raceyear, cdfull)[,-1])
+PAR.data <- cbind(PAR.data, yeardums) #join important covariates with year dummies
 
-View(cbind(cdfull$con_demcandidate[-train.indx], cdfull$con_demshare[-train.indx], pred))
+lmout = lm(as.formula(paste(yname, "~ .", sep="")), data=PAR.data[train.indx,]) #fit
+pred.lm = predict(lmout, newdata=PAR.data[-train.indx,]) #predict on test set
+error.lm = pred.lm - y[-train.indx] #error
+terror.lm = mean(error.lm^2) #MSE
+r_sq.lm <- 1- ( sum(error.lm^2) / sum((y[-train.indx] - mean(y[-train.indx]))^2) ) #r squared
 
-cat("Lasso test error is ", terror.lasso, "\n")
-cat("Ridge test error is ", terror.ridge, "\n")
-cat("Lm test error is ", terror.lm, "\n")
+pred.ridge <- as.vector(pred.ridge)
+pred.lasso <- as.vector(pred.lasso)
+error.ridge <- as.vector(error.ridge)
+error.lasso <- as.vector(error.lasso)
 
-# Now fit the model in the entire data, first choosing between lasso and ridge
-minerr = min(c(terror.lasso, terror.ridge, terror.lm))
-
-if((terror.lasso == minerr))   {
-  fit = "lasso"
-  out = glmnet ( x, y, alpha =1)
-  pred = predict (out, s =  cvout.lasso$lambda.min, newx=x, exact =TRUE, x=x, y=y, type="response")
-  err  = y - pred    # when err>0, y>pred, and you outperformed the prediction
-  coef = predict (out, s =  cvout.lasso$lambda.min, type ="coefficients")
-  included.covars.lasso = names(coef[coef[,1]!=0,])
-  cat("Chosen fit is lasso and the included covariates are \n")
-  print(included.covars.lasso)
-  
-} else if ((terror.ridge  == minerr)) {
-  fit = "ridge"
-  out = glmnet (x , y , alpha = 0 )
-  pred  = predict(out , s = cvout.ridge$lambda.min, newx = x, exact =TRUE, x=x, y=y, type="response")
-  err  = y - pred 
-  cat("Chosen fit is ridge \n")
-} else {
-  fit = "lm"
-  out = lm(as.formula(paste(yname, "~ .", sep="")), data=data)
-  pred = predict (out, newdata=data, type="response")
-  err  = y - pred 
-  cat("Chosen fit is lm \n")
-}
-
-if( nrow(pred) == nrow(cdfull) & ncol(pred) == 1 )  pred = pred[,1] else stop("pred is not a column vector")
-if( nrow(err)  == nrow(cdfull) & ncol(err)  == 1  ) err  =  err[,1] else stop("err  is not a column vector")
-
-rows <- nrow(cdfull)
-pred <- as.vector(pred)
-err <- as.vector(err)
-
-write.csv(cbind(data.frame(cdfull), pred, err, terror.lasso, terror.ridge,  terror.lm, fit, rows), 
-          file = paste("working/predicted-mv.csv", sep=""))
+write.csv(cbind(data.frame(cdfull), pred.lm, pred.ridge, pred.lasso, error.lm, error.ridge, error.lasso),
+          file = paste("../results/demvoteshare_estimation/demvoteshare_predictions.csv", sep=""),
+          row.names = F)
 
 ###
 
-pdf(file = "../results/voteshare_prediction/lasso_MSE.pdf", width = 8, height = 6)
-plot(cvout.lasso)
+pdf(file="../results/demvoteshare_estimation/PARerror_histogram.pdf", width = 10, height = 6)
+par(mfrow=c(1, 3))
+hist(error.lm, breaks = 50, xlab = "OLS", main="")
+hist(error.ridge, breaks = 50, xlab = "Ridge", main="PAR Error Distribution") 
+hist(error.lasso, breaks = 50, xlab = "Lasso", main="")
 dev.off()
 
-out = glmnet ( x, y, alpha =1) # lasso
-pred = predict (out, s =  cvout.lasso$lambda.min, newx=x, exact =TRUE, x=x, y=y, type="response")
-err  = y - pred    # when err>0, y>pred, and you outperformed the prediction
-coef = predict (out, s =  cvout.lasso$lambda.min, type ="coefficients")
 
-pdf(file = "../results/voteshare_prediction/lasso_coefs.pdf", width = 8, height = 6)
-dotplot(coef[abs(coef[,1])>1,], xlab = sprintf("Coefficients with abs. value >1 (%s total nonzero coefficients)", sum(coef[,1]>0)))
-dev.off()
 
-###
 
-pdf(file = "../results/voteshare_prediction/ridge_MSE.pdf", width = 8, height = 6)
-plot(cvout.ridge)
-dev.off()
 
-###
 
-result <- read.csv("working/predicted-mv.csv", stringsAsFactors = F)
-
-result <- result[order(result$err, decreasing = T), ]
-
-result <- result[, c("statenm", "con_raceyear", "con_demcandidate", "err", "con_demshare", "pred")]
-
-library(xtable)
-
-print(xtable(result[1:50,], type="latex"), file="../results/voteshare_prediction/top50residuals.tex")
-
-pdf(file = "../results/voteshare_prediction/parError.pdf", width = 8, height = 6)
-hist(result$err, main=" ", xlab = "Actual - Predicted")
-dev.off()
 
 
 
